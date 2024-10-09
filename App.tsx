d5321a611b20bafdf0b87fedf6997a655d647f4a
Luis Morales Larrrea <luirimo@hotmail.com>
2024-10-08 22:32:10 -0500

    add

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from "react";
+import { PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { StackNavigator } from './src/navigator/StackNavigator';
 
-export default function App() {
+export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <PaperProvider>
+        <StackNavigator></StackNavigator>
+      </PaperProvider>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/config/firebaseConfig.tsx
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkjMPR3iuDinWQNi1M8ieyWRb6qHm8xQ0",
+  authDomain: "compnucleo3-d5440.firebaseapp.com",
+  databaseURL: "https://compnucleo3-d5440-default-rtdb.firebaseio.com",
+  projectId: "compnucleo3-d5440",
+  storageBucket: "compnucleo3-d5440.appspot.com",
+  messagingSenderId: "79533993678",
+  appId: "1:79533993678:web:74c989f85aa61893876a9f"
+};
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+//export const auth = getAuth(firebase);
+export const auth = initializeAuth(firebase, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+export const dbRealTime = getDatabase(firebase);
